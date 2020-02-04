a4a32071e778d62fba4b681a2abc595c694b85aa
fuzzymoggle <jessicacafarelli@gmail.com>
2020-02-04 16:02:37 -0500

    Trying out another tutorial
    Trying out another tutorial
    https://dev.to/hugoliconv/lets-build-a-todo-app-using-vuejs-52in

--- js/main.js
+++ js/main.js
@@ -1,23 +1,67 @@
+// Fucked up code 
+//let app = new Vue({
+//     el: "#vueApp",
+//     data: {
+//         welcomeMessage: 'My To Do List',
+//         lists:[{id1: 'fhskda'}],
+//         newItem: '',
+
+//     },
+//     methods: {
+//         addItem: function(){
+//             let id = this.lists.length + 1
+//             for(let i = 0; i < this.list.length; i++){
+//                 if (this.newItem !== ''){
+//                     const newList = {is:is,item:this.newItem}
+//                     this.list.push(newList)
+//                     this.newItem = ''
+//             }
+
+//             }
+//         }
+//     },
+//     mounted() {
+
+//     },
+// });
+
+// Original code
+// let app = new Vue({
+//     el: "#vueApp",
+//     data: {
+//         welcomeMessage: 'Hello World!'
+//     },
+//     methods: {
+
+//     },
+//     mounted() {
+
+//     },
+// });
+
 let app = new Vue({
     el: "#vueApp",
     data: {
         welcomeMessage: 'My To Do List',
-        lists:[{id1: 'fhskda'}],
-        newItem: '',
-
+        todos: [],
+        addTodoMessage: ''
     },
-    methods: {
-        addItem: function(){
-            let id = this.lists.length + 1
-            for(let i = 0; i < this.list.length; i++){
-                if (this.newItem !== ''){
-                    const newList = {is:is,item:this.newItem}
-                    this.list.push(newList)
-                    this.newItem = ''
-            }
 
-            }
-        }
+    methods: {
+        addTodo(event){
+            const text = event.target.value
+            this.todos.push({text, done: false, id: Date.now()})
+            event.target.value = ''
+        },
+
+        removeTodo(id) {
+            this.todos = this.todos.filter(todo => todo.id !== id)
+        },
+
+        check(todo){
+            todo.done = !todo.done
+        },
+
     },
     mounted() {
 
