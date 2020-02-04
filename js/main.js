82dc7314072df2efea3f16ccafb39499812b854b
Jess Cafarelli <jesscafarelli@Jesss-MacBook-Air.local>
2020-02-03 20:10:17 -0500

    first commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 let app = new Vue({
     el: "#vueApp",
     data: {
-        welcomeMessage: 'Hello World!'
+        welcomeMessage: 'My To Do List',
+        lists:[{id1: 'fhskda'}],
+        newItem: '',
+
     },
     methods: {
+        addItem: function(){
+            let id = this.lists.length + 1
+            for(let i = 0; i < this.list.length; i++){
+                if (this.newItem !== ''){
+                    const newList = {is:is,item:this.newItem}
+                    this.list.push(newList)
+                    this.newItem = ''
+            }
 
+            }
+        }
     },
     mounted() {
 
